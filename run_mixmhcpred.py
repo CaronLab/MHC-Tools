70e6ad4ab2b635be1186977039c6427c0a87e27f
Ruimin Wang <ruimin.wang@yale.edu>
2025-07-02 18:04:55 -0400

    [Fix] change default length range of MixMHCpred

--- run_mixmhcpred.py
+++ run_mixmhcpred.py
@@ -21,5 +21,5 @@ def run_mixmhcpred(min_seq_length, max_seq_length, mhc_class='I'):
 
 
 if __name__ == '__main__':
-    run_mixmhcpred(8, 15, 'I')
+    run_mixmhcpred(8, 14, 'I')
     print('Have a nice day.')
